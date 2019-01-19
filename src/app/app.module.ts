63b44ddab024a40b628cba15727155507f2a421b
p312734 <oliver.balb@gmx.de>
2019-01-19 23:22:40 +0100

    2. Änderung

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { MyComponent } from './app.component.1';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './app.testcomponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.testcomponent.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-component',
+  template: '<h1>Hello World!</h1>'
+})
+export class TestComponent {}
